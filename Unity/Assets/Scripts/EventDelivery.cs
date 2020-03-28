df72f88996833ec57a2394bc2050fc61811f2996
wizardCRain <895240371@qq.com>
2020-03-28 15:53:45 +0800

    Unity 工程初建 添加 事件传递脚本

---
+++ Unity/Assets/Scripts/EventDelivery.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventDelivery : MonoBehaviour, IPointerUpHandler, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    public delegate void LongPressCallBack(GameObject interactive, float pressTime);
+    public event LongPressCallBack LongPress;
+    [SerializeField] private float m_longPressTime = 0.0f;
+
+    private bool m_longPress = false;
+    private float m_timer = 0.0f;
+    private bool inDragging = false;
+
+    private GameObject m_press;
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        m_longPress = false;
+
+        if (inDragging)
+        {
+            return;
+        }
+
+        if (m_timer < m_longPressTime)
+        {
+            Execute(eventData, ExecuteEvents.pointerClickHandler);
+        }
+        else
+        {
+            Execute<ILongPressHandler>(eventData);
+        }
+        m_press = null;
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        m_press = FindCanInteractiveGameObject(eventData);
+        m_timer = 0.0f;
+        m_longPress = true;
+    }
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        inDragging = true;
+        m_press = FindCanInteractiveGameObject(eventData);
+        Execute(eventData, ExecuteEvents.beginDragHandler);
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData) => Execute(eventData, ExecuteEvents.dragHandler);
+
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        Execute(eventData, ExecuteEvents.endDragHandler);
+        inDragging = false;
+        m_press = null;
+    }
+
+    private void Update()
+    {
+        if (!m_longPress)
+        {
+            return;
+        }
+        m_timer += Time.deltaTime;
+    }
+
+    private GameObject FindCanInteractiveGameObject(PointerEventData eventData)
+    {
+        var list = new List<RaycastResult>(5);
+        EventSystem.current.RaycastAll(eventData, list);
+        foreach (var item in list)
+        {
+            var comp = item.gameObject.GetComponent<IEventSystemHandler>();
+            if (!item.gameObject.activeInHierarchy || item.gameObject == gameObject || comp == null || !((MonoBehaviour)comp).enabled)
+            {
+                continue;
+            }
+            return item.gameObject;
+        }
+        return default;
+    }
+
+    private void Execute<T>(PointerEventData eventData, ExecuteEvents.EventFunction<T> fun = null) where T : IEventSystemHandler
+    {
+        if (m_press == null)
+        {
+            return;
+        }
+
+        var eventType = typeof(T);
+
+        if (eventType == typeof(IPointerClickHandler))
+        {
+            var release = FindCanInteractiveGameObject(eventData);
+            if (m_press != release)
+            {
+                return;
+            }
+        }
+
+        if (eventType == typeof(ILongPressHandler))
+        {
+            LongPress?.Invoke(m_press, m_timer);
+        }
+        else if (fun != null)
+        {
+            ExecuteEvents.Execute(m_press, eventData, fun);
+        }
+    }
+}
---
+++ Unity/Assets/Test/Scripts/TestEventDelivery.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TestEventDelivery : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public EventDelivery delivery;
+    public int index;
+
+
+    private void Start()
+    {
+        delivery.LongPress += CallBack;
+    }
+
+    private void CallBack(GameObject go, float pressTime)
+    {
+        if (go != gameObject)
+        {
+            return;
+        }
+        Debug.LogError($"{index}: long press time -> {pressTime}");
+    }
+
+    private void OnDestroy()
+    {
+        delivery.LongPress -= CallBack;
+    }
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.LogError($"{index}: Pointer Click");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.LogError($"{index}: Begin Drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.LogError($"{index}: Dragging");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.LogError($"{index}: End Drag");
+    }
+}
